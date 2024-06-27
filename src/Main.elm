b7ebde38ebe31aa733ef33cd56dc4a5cda42ca46
giacomocerquone <giacomo.cerquone@prima.it>
2024-06-27 11:42:19 +0200

    feat: add delete action

--- src/Main.elm
+++ src/Main.elm
@@ -5,17 +5,18 @@ import Html exposing (Html, div, input, text, button, ul, li)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import Html.Events exposing (onClick)
+import Html.Events exposing (onSubmit)
 
 -- MAIN
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
 
-
 -- MODEL
 type alias Model =
-  { tasks : List String,
-  task: String
+  { 
+    tasks : List String,
+    task: String
   }
 
 
@@ -27,20 +28,21 @@ init =
   }
 
 
-
 -- UPDATE
 type Msg
   = Change String
   | AddTask
+  | DeleteTask String
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Change newContent ->
-      { model | task = newContent }
+    Change newTask ->
+      { model | task = newTask }
     AddTask ->
       { task = "", tasks = model.task :: model.tasks }
-
+    DeleteTask taskToDelete ->
+      { model | tasks = List.filter (\t -> t /= taskToDelete) model.tasks }
 
 
 -- VIEW
@@ -48,11 +50,20 @@ view : Model -> Html Msg
 view model =
   div []
   [
-    div [ style "display" "flex" ]
-      [ input [ placeholder "Add a task", value model.task, onInput Change ] []
-      , button [onClick AddTask] [ text "Add"] 
+    Html.form [ style "display" "flex", onSubmit AddTask ]
+      [ input [ placeholder "Add a task", value model.task, onInput Change ] [],
+        button [type_ "submit" ] [text "Add"] 
       ]
     , ul []
-      (List.map (\t-> li [] [text t]) model.tasks)
+      (
+        List.map 
+          (\t -> li [] 
+            [
+              text (t ++ " - "),
+              button [onClick (DeleteTask t)] [text "Delete"]
+            ]
+          )
+        model.tasks
+      )
   ]
 
